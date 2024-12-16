ee2a151a77b9838e0a8cd6248013a7cbf0e36f0a
Emin Polat <51139204+eminpolat749@users.noreply.github.com>
2024-12-16 17:16:59 +0300

    core annotations

---
+++ 1-SomeCoreAnnotations/src/main/java/org/csystem/app/component/Sample.java
@@ -0,0 +1,19 @@
+package org.csystem.app.component;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+@Component("emin")
+public class Sample {
+    public Sample()
+    {
+        System.out.println("I am a default ctor of Sample");
+    }
+
+    @PostConstruct
+    public void foo()
+    {
+        System.out.println("foo");
+    }
+}
+// buradaki foo metodu Sample nesnesi yaratıldıktan sonra çağrılacak
