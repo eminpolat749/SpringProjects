456907f7ce4f8fa64353a1dbe3188a23d14dd453
Emin Polat <51139204+eminpolat749@users.noreply.github.com>
2025-01-07 18:32:47 +0300

    repository layer added

---
+++ PaymentSystem/PaymentRepositoryLib/src/main/java/org/csystem/app/payment/repository/entity/Customer.java
@@ -0,0 +1,22 @@
+package org.csystem.app.payment.repository.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+    @Id
+    public String username;
+    @Column(nullable = false, length = 512)
+    public String name;
+    @Column(name = "register_date", nullable = false)
+    public LocalDate registerDate;
+
+    public boolean active;
+
+    @OneToMany
+    public Set<Payment> payments;
+}
